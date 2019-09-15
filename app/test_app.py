0a6a12675e6980966e6802209257214f63870d8a
Brandon Serna <brandonsserna@gmail.com>
2019-09-14 22:42:01 -0600

    Updated badge and added sample mit license ☀︎

--- app/test_app.py
+++ app/test_app.py
@@ -8,6 +8,8 @@ def client():
     client = app.test_client()
     yield client
 
+
 def test_root(client):
-    r = client.get('/')
-    assert b'Hello world' == r.data
+    r = client.get("/")
+    assert b"Hello world" == r.data
+
